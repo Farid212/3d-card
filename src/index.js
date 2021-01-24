4725afd7457fd576df42825bc685f3f43cf8767d
Farid <farid.hammouten@gmail.com>
2021-01-24 18:13:50 +0900

    other shit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import Card from './components/Card';
 import reportWebVitals from './reportWebVitals';
+import theImage from './assets/images/me.png';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Card
+      image={theImage}
+      imageName="my image"
+      cardTitle="Farid Hammouten"
+      subTitle="Web Developer"
+      medias={{
+        twitter: "https://twitter.com/Faridh212",
+        github: "https://github.com/Farid212",
+        medium: "https://medium.com/@farid212",
+        external: "https://www.farid212.dev/"
+      }}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
